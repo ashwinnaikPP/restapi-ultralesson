516e41b1ecf05d6586d7d27b7e5548395eb30e4b
Ashwin Naik <ashwinnaik94@gmail.com>
2023-01-20 21:38:12 +0530

    Added smaple TestNG tests

---
+++ src/test/java/SampleTests.java
@@ -0,0 +1,11 @@
+import org.testng.Assert;
+import org.testng.annotations.Test;
+import org.testng.asserts.Assertion;
+
+public class SampleTests {
+
+    @Test
+    public void shouldTestSomething(){
+        Assert.assertEquals(1,1);
+    }
+}

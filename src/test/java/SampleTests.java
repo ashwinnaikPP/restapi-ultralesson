1d7af53ad811f132506dde283ad7343aaf04c490
Ashwin Naik <ashwinnaik94@gmail.com>
2023-01-23 20:09:42 +0530

    Added test invoking GetAllUsers API

--- src/test/java/SampleTests.java
+++ src/test/java/SampleTests.java
@@ -2,6 +2,8 @@ import org.testng.Assert;
 import org.testng.annotations.Test;
 import org.testng.asserts.Assertion;
 
+import static io.restassured.RestAssured.given;
+
 public class SampleTests {
 
     @Test
@@ -12,5 +14,20 @@ public class SampleTests {
     @Test
     public void secondSampleTest(){
         Assert.assertEquals(5.3,5.3);
+    }
+
+    @Test
+    public void shouldGetAllTheUsers() {
+        // 1. Arrange
+        // 2. Act
+        // 3. Assertion
+
+        given()
+                .when()
+                    .get("https://gorest.co.in/public/v2/users")
+                .then()
+                    .statusCode(200)
+                    .log().body();
+
     }
 }
